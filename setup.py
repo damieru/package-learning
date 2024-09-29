f14ddec9186bc7fdbe48a187dcd06c03225ab591
daniel <damieru@gmail.com>
2024-09-29 18:37:30 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='greets', 
+	version='0.1.0',
+	description='A python example package', 
+	author='Daniel Albuquerque',
+	author_email='damieru@gmail.com',
+	license='None',
+	packages=['greets'],
+	classifiers=[
+		'Programming Language :: Python :: 3'
+	]
+)
